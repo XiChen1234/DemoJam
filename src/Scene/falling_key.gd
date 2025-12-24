edfd25e5b20accaf05aa0c1c2948ae0322206118
XiChen1234 <xichen888.private@gmail.com>
2025-12-24 20:30:01 +0800

    feat: v0.3

--- src/Scene/falling_key.gd
+++ src/Scene/falling_key.gd
@@ -1,7 +1,24 @@
 class_name FallingKey
 extends Node2D
 
+enum TYPE {
+	LEFT_CLICK, RIGHT_CLICK, LEFT_LONG_PRESS, RIGHT_LONG_PRESS,ELSE
+}
+
+const TYPE_TEXTURES = {
+	TYPE.LEFT_CLICK: preload("res://Assert/Art/red-arrows.png"),
+	TYPE.RIGHT_CLICK: preload("res://Assert/Art/blue-arrows.png"),
+	TYPE.LEFT_LONG_PRESS: preload("res://Assert/Art/red-line.png"),
+	TYPE.RIGHT_LONG_PRESS: preload("res://Assert/Art/blue-line.png"),
+	TYPE.ELSE: preload("res://Assert/Art/arrow.png"),
+}
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 @export var speed: float = 1000
+@export var type: TYPE
+@export var timestamp: float
+@export var duration: float # 单击为默认0，长按不为零
 
 var init_position: Vector2 = Vector2(2100, 200)
 
@@ -14,6 +31,18 @@ func _process(delta: float) -> void:
 	# 如果超出屏幕边缘，则回收
 	if position.x < -100:
 		recycle()
+
+
+"""设置纹理"""
+func set_texture() -> void:
+	var texture: Resource = TYPE_TEXTURES[type]
+	match type:
+		TYPE.LEFT_CLICK, TYPE.RIGHT_CLICK:
+			sprite_2d.texture = texture
+		TYPE.LEFT_LONG_PRESS, TYPE.RIGHT_LONG_PRESS:
+			# 暂时用这个箭头占位
+			sprite_2d.texture = TYPE_TEXTURES[TYPE.ELSE]
+
 
 """回收/重置音符"""
 func recycle() -> void:
--- src/Scene/music_game_area.gd
+++ src/Scene/music_game_area.gd
@@ -1,25 +1,32 @@
 extends Node2D
 
 @onready var falling_key = preload("res://Scene/falling_key.tscn")
-@onready var test_random_fall_key_timer: Timer = $TestRandomFallKeyTimer
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 @export var pool_size: int = 10
+@export var level_file_path: String = "res://Level/level_1/level_1.json"
 
 var fk_pool: Array[FallingKey] = []
 var init_position: Vector2 = Vector2(2100, 200)
 
-# test: 随机时间计时器
-@export var spawn_time: Vector2 = Vector2(0.5, 1.5)
-
+## 关卡相关
+var level_data: Array = []
+var level_count: int = 0
+var falling_key_index: int = 0
 
 func _ready() -> void:
+	load_level_data()
+	
 	audio_stream_player.play()
 	
 	initialize_pool()
-	set_random_timer()
 
 
+func _process(_delta: float) -> void:
+	gene_falling_key()
+
+
+"""获取音乐内时间戳"""
 func get_music_time() -> float:
 	return audio_stream_player.get_playback_position() + AudioServer.get_time_since_last_mix()
 
@@ -44,36 +51,60 @@ func right_long_press(duration: float):
 	print("右键长按 - 时长: %f 秒 - 音乐结束时间戳: %f 秒" % [duration, timestamp])
 
 
-"""初始化随机时间的计时器"""
-func set_random_timer():
-	# 生成随机时间间隔
-	var timer = randf_range(spawn_time[0], spawn_time[1])
-	# 设置定时器的等待时间
-	test_random_fall_key_timer.wait_time = timer
-	# 重启定时器
-	test_random_fall_key_timer.start()
+## 关卡部分
+"""时间戳对比检测"""
+func judge_fall_key() -> void:
+	# todo
+	pass
 
 
-"""Test: demo定时生成音符"""
-func _on_create_falling_key() -> void:
-	var fk_inst: FallingKey = get_from_pool()
-	# 激活
-	activate_falling_key(fk_inst)
+"""生成音符"""
+func gene_falling_key() -> void:
+	if falling_key_index >= level_count:
+		return
+	var current_time: float = get_music_time()
+	var timestamp: float = level_data[falling_key_index].get("timestamp")
+	if timestamp < current_time:
+		print(level_data[falling_key_index].get("name"))
+		var type = level_data[falling_key_index].get("type")
+		var duration = level_data[falling_key_index].get("duration")
+		var fk_inst: FallingKey = get_from_pool(type, timestamp, duration)
+		
+		activate_falling_key(fk_inst)
+		falling_key_index += 1
 
 
+"""加载关卡数据"""
+func load_level_data() -> void:
+	var file: FileAccess = FileAccess.open(level_file_path, FileAccess.READ)
+	if file:
+		var json_string: String = file.get_as_text()
+		var json_data: Variant = JSON.parse_string(json_string)
+		level_data = json_data.get("timeline")
+		level_count = level_data.size()
+
+
+## 对象池部分
 """激活对象"""
 func activate_falling_key(fk_inst: FallingKey) -> void:
 	# 重置音符状态
 	fk_inst.visible = true
 	fk_inst.set_process(true)
 	fk_inst.set_physics_process(true)
-	set_random_timer()
 
 
 """从对象池中获取对象"""
-func get_from_pool() -> FallingKey:
+func get_from_pool(
+	p_type: FallingKey.TYPE, 
+	p_timestamp: float, 
+	p_duration: float = 0) -> FallingKey:
+	
 	for fk_inst in fk_pool:
 		if not fk_inst.visible:
+			fk_inst.type = p_type
+			fk_inst.timestamp = p_timestamp
+			fk_inst.duration = p_duration 
+			fk_inst.set_texture()
 			return fk_inst
 	
 	return expand_pool()
@@ -95,7 +126,6 @@ func initialize_pool() -> void:
 		# 添加到场景和对象池
 		add_child(fk_inst)
 		fk_pool.append(fk_inst)
-
 
 """创建音符"""
 func create_falling_key() -> FallingKey:
