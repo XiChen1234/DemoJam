5f49bc08c0a00aac18f33fdcc3fa61356280c9ee
XiChen1234 <xichen888.private@gmail.com>
2025-12-29 00:53:17 +0800

    feat: delete useless pic and add level new pic
    删除无用素材图片，增加关卡美术资源
    关卡做了一半，放弃了，再构思构思

--- src/Scene/UI/select_level.gd
+++ src/Scene/UI/select_level.gd
@@ -2,11 +2,8 @@ extends Control
 
 @onready var level: TextureButton = $HBoxContainer/Level
 
+var level_array: Array = []
+
 
 func _on_back() -> void:
 	get_tree().change_scene_to_file("res://Scene/UI/title_scene.tscn")
-
-
-
-func _on_level_selected() -> void:
-	get_tree().change_scene_to_file("res://Scene/Game/game_scene.tscn")
