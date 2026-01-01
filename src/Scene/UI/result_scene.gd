1aaaf81117a4fed55c6f904fb91c86f04fd08932
XiChen1234 <xichen888.private@gmail.com>
2026-01-01 20:22:22 +0800

    feat: 完善评级逻辑

--- src/Scene/UI/result_scene.gd
+++ src/Scene/UI/result_scene.gd
@@ -1,10 +1,22 @@
 extends Control
 
+enum Rank {
+	S, A, B, C, D
+}
+const RankText: Dictionary[Rank, String] = {
+	Rank.S: "Super",
+	Rank.A: "A",
+	Rank.B: "B",
+	Rank.C: "C",
+	Rank.D: "D",
+}
+
 @onready var score_label: Label = $ScoreLabel
 @onready var combo_label: Label = $ComboLabel
 @onready var perfect_label: Label = $PerfectLabel
 @onready var great_label: Label = $GreatLabel
 @onready var miss_label: Label = $MissLabel
+@onready var rank_label: Label = $RankLabel
 
 
 func _ready() -> void:
@@ -22,6 +34,31 @@ func _render_result(result: GameResult) -> void:
 	perfect_label.text = "Perfect: %d" % result.perfect_count
 	great_label.text = "Great: %d" % result.great_count
 	miss_label.text = "Miss: %d" % result.miss_count
+	rank_label.text = "Rank: %s" % RankText[_calculate_rank(result)] 
+
+
+"""计算评级函数"""
+func _calculate_rank(result: GameResult) -> Rank:
+	var total_notes := result.perfect_count \
+		+ result.great_count \
+		+ result.miss_count
+
+	if total_notes == 0:
+		return Rank.D
+
+	var accuracy := float(result.perfect_count * 2 + result.great_count) \
+		/ float(total_notes * 2)
+
+	if accuracy >= 0.95:
+		return Rank.S
+	elif accuracy >= 0.85:
+		return Rank.A
+	elif accuracy >= 0.70:
+		return Rank.B
+	elif accuracy >= 0.50:
+		return Rank.C
+	else:
+		return Rank.D
 
 
 func _on_next() -> void:
