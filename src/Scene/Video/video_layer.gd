5acdfd4feea279d0f45d3b469ec30f99e089c487
XiChen1234 <xichen888.private@gmail.com>
2026-01-05 00:59:36 +0800

    feat: add video layer
    增加视频播放层，为片头片尾做准备
    :

---
+++ src/Scene/Video/video_layer.gd
@@ -0,0 +1,38 @@
+extends Control
+
+@onready var video_stream_player: VideoStreamPlayer = $VideoStreamPlayer
+@onready var skip: Button = $Skip
+
+@export var video_stream: VideoStream
+# 下一个场景
+@export var next_scene: PackedScene
+
+
+func _ready() -> void:
+	video_stream_player.play()
+
+
+"""去往下一个场景"""
+func _goto_next_scene() -> void:
+	if next_scene:
+		# 实例化新场景
+		var new_scene = next_scene.instantiate()
+		# 替换当前场景
+		if get_tree().current_scene:
+			get_tree().current_scene.queue_free()
+		get_tree().root.add_child(new_scene)
+		get_tree().current_scene = new_scene
+
+
+"""
+视频播放结束
+"""
+func _on_video_finished() -> void:
+	_goto_next_scene()
+
+
+"""跳过"""
+func _on_skip() -> void:
+	if video_stream_player.is_playing():
+		video_stream_player.stop()
+	_goto_next_scene()
